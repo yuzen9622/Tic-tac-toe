375cc100099b7f9f3bc987707eb323e12cb54ab0
yuzen622 <125558430+yuzen622@users.noreply.github.com>
2024-09-21 12:43:28 +0800

    change url

--- src/servirce.jsx
+++ src/servirce.jsx
@@ -1,2 +1,2 @@
-//export const url = "https://tic-tac-toe-socket-server.onrender.com";
-export const url = "http://localhost:5000";
+export const url = "https://tic-tac-toe-socket-server.onrender.com";
+//export const url = "http://localhost:5000";
