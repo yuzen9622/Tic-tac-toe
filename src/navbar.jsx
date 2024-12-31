f958978cfe51d6db478488462db027e52520f87d
yuzen622 <125558430+yuzen622@users.noreply.github.com>
2024-12-31 15:49:15 +0800

    public to local

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -2,17 +2,27 @@ import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import "./navbar.css";
 import { UserContext } from "./userContext";
+/**
+ *  導航欄
+ * @returns {React.JSX.Element} Navbar
+ */
 export default function Navbar() {
+  /**
+   * useContext
+   * @type {Object} user 當前使用者資料
+   * @type {Function} logout 登出函式
+   */
   const { user, logout } = useContext(UserContext);
 
   return (
     <div className="navbar">
+      {/* 所有用戶皆可見的導航連結 */}
       <NavLink to={"/"}>首頁</NavLink>
       <NavLink to={"/online"}>線上</NavLink>
       <NavLink to={"/single"}>單機</NavLink>
       <NavLink to={"/AI"}>人機</NavLink>
       <NavLink to={"/record"}>排行榜</NavLink>
-
+      {/* 根據用戶是否登入進行條件渲染 */}
       {user ? (
         <>
           <NavLink to={"/history"}>歷史</NavLink>
